9496eaeb7498235b5dc4e70eafee3c4372c33897
Talha Akhoon <talha.akhoon@outlook.com>
2025-10-28 00:46:10 +0000

    homework 5 submitted

---
+++ homework-05/README.md
@@ -0,0 +1,5 @@
+# Building the container
+`docker build -t hwk5model .`
+
+# Running the container
+`docker run --rm -p 9000:9000 hwk5model`
---
+++ homework-05/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import pickle
+
+app = FastAPI()
+
+with open('pipeline_v1.bin', 'rb') as f:
+    pipeline = pickle.load(f)
+
+@app.post("/predict")
+def predict(client: dict):
+    """
+    Predict conversion probability for a client
+    """
+    # Make prediction
+    prediction_proba = pipeline.predict_proba([client])[0, 1]
+    
+    return {
+        "conversion_probability": float(prediction_proba),
+        "conversion_probability_rounded": round(float(prediction_proba), 3)
+    }
+
